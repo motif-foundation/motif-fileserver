d362f0a3c910629d84c0ed52b1116bd4da6c8fe5
Steve <johnw@motif.foundation>
2022-07-07 14:25:35 +0300

    update

---
+++ README.md
@@ -0,0 +1,14 @@
+# Motif file server
+
+## Use
+used to store different size thumbs
+
+##Preq
+install node-static globally
+install nginx 
+allow port 8040
+go to files folder
+static -a 0.0.0.0 -p 8040
+
+## Run
+node index.js
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const os = require('os');
+const _ = require('lodash');
+
+const app = express();
+
+const multer  = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './files/images/')
+  },
+  filename: function (req, file, cb) { 
+      cb(null,  file.originalname ); 
+  }
+});
+var upload = multer({ storage: storage }); 
+
+app.post('/upload', upload.single('file'), function(req, res) {
+  const title = req.body.title;
+  const file = req.file; 
+  res.sendStatus(200);
+});
+ 
+const port = process.env.PORT || 4077;
+app.listen(port, () => 
+  console.log(`Motif fileserver is listening on port ${port}.`)
+);
